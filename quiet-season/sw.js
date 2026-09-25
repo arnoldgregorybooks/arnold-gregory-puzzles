9275055c18fcdecb961581101967cb92d97bfc19
arnoldgregorybooks <arnoldstel2016@gmail.com>
2026-09-25 16:11:14 +0200

    Add sw.js (Quiet Season puzzle collection)

---
+++ quiet-season/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "quiet-season-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./data-sudoku.js",
+  "./data-wordsearch.js",
+  "./data-bonus.js",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if(cached) return cached;
+      return fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy)).catch(() => {});
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
